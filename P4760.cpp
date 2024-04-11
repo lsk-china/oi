a3e084d91843a5ae7534d6c0392665da8d186560
lsk <colaleo225@gmail.com>
2024-04-11 13:19:30 +0800

    P4760

--- P4760.cpp
+++ P4760.cpp
@@ -9,7 +9,6 @@
 
 #define pii std::pair<int, int>
 #define mkpii(a, b) std::make_pair(a, b)
-#define square(a) a*a
 
 struct wheel {
     int x, y, radius;
@@ -31,6 +30,10 @@ pii speed(struct wheel master, struct wheel slave) {
     return simplify(mkpii(a, b));
 }
 
+int square(int a) {
+    return a*a;
+}
+
 bool intersect(struct wheel wheel1, struct wheel wheel2) {
     int a = square(wheel1.radius + wheel2.radius);
     int b = square(wheel1.x - wheel2.x) + square(wheel1.y - wheel2.y);
@@ -41,9 +44,11 @@ void solve() {
     int wheelAmount;
     scanf("%d", &wheelAmount);
     bool* visited = (bool*) malloc(sizeof (bool) * wheelAmount);
+    memset(visited, false, sizeof (bool ) * wheelAmount);
     struct wheel* wheels = (struct wheel *) malloc(sizeof (struct wheel) * wheelAmount);
     for (int i = 0; i < wheelAmount; i++) {
         scanf("%d %d %d", &wheels[i].x, &wheels[i].y, &wheels[i].radius);
+        wheels[i].id = i;
     }
     wheels[0].vel = mkpii(1, 1);
     wheels[0].direction = true; // clockwise
@@ -55,7 +60,7 @@ void solve() {
             struct wheel &thisWheel = wheels[i];
             if (!visited[i] && intersect(top, thisWheel)) {
                 visited[i] = true;
-                thisWheel.vel = speed(top, thisWheel);
+                thisWheel.vel = speed(  top, thisWheel);
                 thisWheel.direction = ! top.direction;
                 queue.push(thisWheel);
             }
@@ -64,9 +69,9 @@ void solve() {
     for (int i = 0; i < wheelAmount; i++) {
         if (visited[i]) {
             if (wheels[i].vel.second == 1) {
-                printf("%d %s", wheels[i].vel.first, wheels[i].direction ? "clockwise" : "counterclockwise");
+                printf("%d %s\n", wheels[i].vel.first, wheels[i].direction ? "clockwise" : "counterclockwise");
             } else {
-                printf("%d / %d %s", wheels[i].vel.first, wheels[i].vel.second, wheels[i].direction ? "clockwise" : "counterclockwise");
+                printf("%d/%d %s\n", wheels[i].vel.first, wheels[i].vel.second, wheels[i].direction ? "clockwise" : "counterclockwise");
             }
         } else {
             printf("not moving\n");
