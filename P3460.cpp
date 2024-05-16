f2a1b170ce267661303c790a08ca9346cca3e045
lsk <colaleo225@gmail.com>
2024-05-16 13:25:36 +0800

    P3460

---
+++ P3460.cpp
@@ -0,0 +1,64 @@
+//
+// Created by lsk on 5/16/24.
+//
+#include <iostream>
+#include <cstdlib>
+#include <stack>
+#include <vector>
+#include <cstring>
+
+struct node {
+    int v; struct node *next;
+};
+
+void freeList(struct node *head) {
+    if (head->next != nullptr) freeList(head->next);
+    free(head);
+}
+
+void swap(struct node *head, int index1, int index2) {
+    struct node *index1Node = head;
+    for (int i = 0; i < index1; i++) index1Node = index1Node->next;
+    struct node *index2Node = head;
+    for (int i = 0; i < index2; i++) index2Node = index2Node->next;
+    // 1 - 2 - 3 - 4 - 5 -> 1 3 2 4 5
+    struct node *index2Next = index2Node -> next;
+}
+
+int main() {
+    int n; std::cin>>n;
+//    int *input = (int *) malloc(sizeof(int) * n * 2);
+//    int *vis = (int *) malloc(sizeof(int) * n);
+//    memset(vis, -1, sizeof(int) * n);
+//    for (int i = 0; i < 2*n; i++) std::cin >> input[i];
+//    std::vector<int> ans;
+//    std::stack<int> stack;
+//    for (int i = 0; i < 2*n; i++) {
+//        if (vis[input[i]] != -1) {
+//            std::swap(input[vis[i]], input[i]);
+//
+//        }
+//    }
+//    printf("%d\n", ans.size());
+//    for (int i : ans) printf("%d\n", i);
+//    free(input);
+//    free(vis);
+    struct node *head = (struct node *) malloc(sizeof(struct node));
+    struct node *end = head;
+    head->next = nullptr;
+    for (int i = 0; i < 2*n; i++) {
+        std::cin >> end->v;
+        end->next = (struct node *) malloc(sizeof(struct node));
+        end = end->next;
+    }
+    int *vis = (int *) malloc(sizeof(int) * n);
+    std::vector<int> ans;
+    for (int i = 0; i < n; i++) {
+        if (vis[i] != -1) {
+
+        }
+    }
+    freeList(head);
+    free(vis);
+    return 0;
+}
