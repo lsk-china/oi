c560da7a51e3553eb72eb83c5d0f98f1c6941cc0
lsk <colaleo225@gmail.com>
2024-05-30 13:24:46 +0800

    P1908

--- P1908.cpp
+++ P1908.cpp
@@ -13,18 +13,46 @@ struct num {
     int i;
 };
 
+void update(int *treearr, int len, int i, int d) {
+    while (i <= len) {
+        treearr[i] += d;
+        i += lowbit(i);
+    }
+}
+
+int query(int *treearr, int i) {
+    int result = 0;
+    while (i > 0) {
+        result += treearr[i];
+        i -= lowbit(i);
+    }
+    return result;
+}
+
 int main() {
     int n; std::cin >> n;
-    struct num *vals = (struct num*) malloc(sizeof(struct num) * n);
-    for (int i = 0; i < n; i++) {
+    struct num *vals = (struct num*) malloc(sizeof(struct num) * (n+1));
+    int *indexes = (int *) malloc(sizeof(int) * (n+1));
+    int *treearr = (int *) malloc(sizeof(int) * (n+1));
+    memset(treearr, 0, sizeof(int) * n);
+    for (int i = 1; i <= n; i++) {
         std::cin >> vals[i].val;
         vals[i].i = i;
     }
-    std::sort(vals+1, vals+n+1, [](struct num n1, struct num n2) {
+    std::sort(vals+2, vals+n+2, [](struct num n1, struct num n2) {
         return n1.val == n2.val ? n1.i < n2.i : n1.val < n2.val;
     });
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
+        indexes[i] = vals[i].i;
     }
+    long long ans = 0;
+    for (int i = 1; i < n+1; i++) {
+        update(treearr, n, indexes[i], 1);
+        ans += i - query(treearr, indexes[i]);
+    }
+    printf("%lld", ans);
     free(vals);
+    free(treearr);
+    free(indexes);
     return 0;
 }
