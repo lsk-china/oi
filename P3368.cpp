e73c6ece1fd6b49b9befc35383ca02d85a9fee25
lsk <colaleo225@gmail.com>
2024-06-12 12:51:48 +0800

    P3368

--- P3368.cpp
+++ P3368.cpp
@@ -17,27 +17,29 @@ void update(int i, int d) {
 
 int query(int i) {
     int result = 0;
-    while (i >= 0) {
+    while (i > 0) {
         result += treearr[i];
         i -= lowbit(i);
     }
+    return result;
 }
 
 int main() {
     int optCount; std::cin >> len >> optCount;
+    input[0] = 0;
+    for (int i = 1; i <= len; i++) std::cin >> input[i];
     for (int i = 1; i <= len; i++) {
-        int a; std::cin >> a;
-        update(i, a - query(i));
+        update(i, input[i]-input[i-1]);
     }
     for (int i = 0; i < optCount; i++) {
         int opt; std::cin >> opt;
         if (opt == 1) {
             int x, y, d; std::cin >> x >> y >> d;
             update(x, d);
-            update(y, -d);
+            update(y+1, -d);
         } else {
-            int i; std::cin >> i;
-            printf("%d\n", query(i));
+            int index; std::cin >> index;
+            printf("%d\n", query(index));
         }
     }
     return 0;
