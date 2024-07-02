0101f771dda3edd4b3166e4fd477bd72ca44f2db
lsk <colaleo225@gmail.com>
2024-07-02 13:15:26 +0800

    P1631 P1168

--- P1168.cpp
+++ P1168.cpp
@@ -1,28 +1,98 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <queue>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <cstring>
+//#include <queue>
+//#include <vector>
+//#include <utility>
+//
+//int main() {
+//    std::priority_queue<int, std::vector<int>, std::less<int>> ql;
+//    int n, v, leftSize=1, rightSize=0, rightSmallest = 2147483647;
+//    scanf("%d", &v);
+//    scanf("%d", &n);
+//    ql.push(v);
+//    for (int i = 2; i <= n; i++) {
+//        scanf("%d", &v);
+//        if (v < rightSmallest && (leftSize - rightSmallest) == 1) {
+//            ql.push(v);
+//            leftSize ++;
+//        } else {
+//            rightSmallest = v;
+//            rightSize ++;
+//        }
+//        if (i % 2 == 1) {
+//            printf("%d\n", ql.top());
+//        }
+//    }
+//    return 0;
+//}
+
+#include <iostream>
 #include <vector>
+#include <algorithm>
 #include <utility>
 
-int main() {
-    std::priority_queue<int, std::vector<int>, std::less<int>> ql;
-    int n, v, leftSize=1, rightSize=0, rightSmallest = 2147483647;
-    scanf("%d", &v);
-    scanf("%d", &n);
-    ql.push(v);
+#define MAX 100010
+
+void solution1() {
+    int n; std::cin >> n;
+    std::vector<int> vec;
+    for (int i = 1; i <= n; i++) {
+        int a; std::cin >> a;
+        vec.insert(std::upper_bound(vec.begin(), vec.end(), a), a);
+        if (i % 2 == 1) std::cout << vec[(vec.size() - 1)/2] << std::endl;
+    }
+}
+
+struct tree {
+    int parent, value, left, right;
+};
+
+struct tree elements[MAX] = {0};
+int input[MAX] = {0};
+int n = 0;
+
+void build(int rootIndex) {
+    struct tree root = elements[rootIndex];
+    root.parent = -1;
+    root.value = input[1];
     for (int i = 2; i <= n; i++) {
-        scanf("%d", &v);
-        if (v < rightSmallest && (leftSize - rightSmallest) == 1) {
-            ql.push(v);
-            leftSize ++;
+        int v = input[i];
+        int index = rootIndex;
+find:   if (v > elements[index].value) {
+            if (elements[index].right == 0) {
+                elements[++index] = {
+                        .parent = index - 1,
+                        .value = v
+                };
+                elements[index - 1].right = index;
+                index --;
+            } else {
+                index = elements[index].right;
+                goto find;
+            }
         } else {
-            rightSmallest = v;
-            rightSize ++;
-        }
-        if (i % 2 == 1) {
-            printf("%d\n", ql.top());
+            if (elements[index].left == 0) {
+                elements[++index] = {
+                        .parent = index - 1,
+                        .value = v
+                };
+                elements[index - 1].left = index;
+                index --;
+            } else {
+                index = elements[index].left;
+                goto find;
+            }
         }
     }
-    return 0;
 }
+
+void solution2() {
+    std::cin >> n;
+}
+
+int main() {
+
+}
+
+
--- P1631.cpp
+++ P1631.cpp
@@ -1,36 +1,71 @@
+////
+//// Created by lsk on 3/20/24.
+////
+//#include <cstdio>
+//#include <cstdlib>
+//#include <queue>
+//#include <vector>
 //
-// Created by lsk on 3/20/24.
+//#define pair_t std::pair<int, int>
 //
-#include <cstdio>
-#include <cstdlib>
+//int main () {
+//    int n;
+//    scanf("%d", &n);
+//    int *seq1 = (int *) malloc(sizeof(int)*n);
+//    int *seq2 = (int *) malloc(sizeof(int)*n);
+//    std::priority_queue<pair_t, std::vector<pair_t>, std::greater<pair_t>> resultQueue;
+//    for(int i = 0; i < n; i++) {
+//        scanf("%d", &seq1[i]);
+//    }
+//    for(int i = 0; i < n; i++) {
+//        scanf("%d", &seq2[i]);
+//    }
+//    for (int i = 0; i < n; i++) {
+//        resultQueue.push(std::make_pair(seq1[i] + seq2[0], 0));
+//    }
+//    for (int i = 0; i < n; i++) {
+//        pair_t head = resultQueue.top();
+//        resultQueue.pop();
+//        printf("%d ", head.first);
+//        head.first;
+//    }
+//    free(seq1);
+//    free(seq2);
+//    printf("\n");
+//    return 0;
+//}
+
+#include <iostream>
+#include <cstring>
 #include <queue>
-#include <vector>
 
-#define pair_t std::pair<int, int>
+#define mkpii(a, b) std::make_pair(a, b)
+#define pii std::pair<int, int>
+#define MAX 100010
 
-int main () {
-    int n;
-    scanf("%d", &n);
-    int *seq1 = (int *) malloc(sizeof(int)*n);
-    int *seq2 = (int *) malloc(sizeof(int)*n);
-    std::priority_queue<pair_t, std::vector<pair_t>, std::greater<pair_t>> resultQueue;
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &seq1[i]);
-    }
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &seq2[i]);
+int seq1[MAX] = {0};
+int seq2[MAX] = {0};
+
+int main() {
+    int n; std::cin >> n;
+    for (int i = 1; i <= n; i++) std::cin >> seq1[i];
+    for (int i = 1; i <= n; i++) std::cin >> seq2[i];
+    std::queue<int> queue;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            int sum = seq1[i] + seq2[j];
+            if (queue.size() < n) {
+                queue.push(sum);
+            } else if(queue.front() > sum) {
+                queue.pop();
+                queue.push(sum);
+            }
+        }
     }
     for (int i = 0; i < n; i++) {
-        resultQueue.push(std::make_pair(seq1[i] + seq2[0], 0));
+        int e = queue.front();
+        queue.pop();
+        printf("%d ", e);
     }
-    for (int i = 0; i < n; i++) {
-        pair_t head = resultQueue.top();
-        resultQueue.pop();
-        printf("%d ", head.first);
-        head.first;
-    }
-    free(seq1);
-    free(seq2);
     printf("\n");
-    return 0;
 }
